959a9040c2bbf1fb06fbacc79ffb7e9b8f3500fb
Debayudh Mitra <78213067+debamitr1012@users.noreply.github.com>
2023-05-08 01:45:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,13 @@
-from flask import Flask,request,jsonify
+import streamlit as st
 import numpy as np
 import pickle
-model = pickle.load(open('C:/Users/91983/mlapp/model.pkl','rb'))
-app = Flask(__name__)
-@app.route('/')
-def index():
-    return "Hello world"
-@app.route('/predict',methods=['POST'])
-def predict():
-    cgpa = request.form.get('cgpa')
-    iq = request.form.get('iq')
-    profile_score = request.form.get('profile_score')
-    input_query = np.array([[cgpa,iq,profile_score]])
+model = pickle.load(open('C:/Users/91983/mlapp/model.pkl', 'rb'))
+st.title('Placement Predictor')
+st.write('Enter the details below to predict your placement status.')
+cgpa = st.text_input('CGPA', '0')
+iq = st.text_input('IQ', '0')
+profile_score = st.text_input('Profile Score', '0')
+if st.button('Predict'):
+    input_query = np.array([[cgpa, iq, profile_score]])
     result = model.predict(input_query)[0]
-    return jsonify({'placement':str(result)})
-if __name__ == '__main__':
-    app.run(debug=True)
+    st.write(f'Placement: {result}')
