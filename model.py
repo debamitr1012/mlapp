3419fc9fc46ac65e29c550ee6df2853cfde0d11d
Debayudh Mitra <78213067+debamitr1012@users.noreply.github.com>
2023-05-08 02:01:53 +0530

    Update model.py

--- model.py
+++ model.py
@@ -6,13 +6,11 @@ X = df.drop(columns=['placed'])
 y = df['placed']
 from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=2)
-#train the model
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 rf = RandomForestClassifier()
 rf.fit(X_train,y_train)
 y_pred = rf.predict(X_test)
 print(accuracy_score(y_test,y_pred))
-#save the model in pickle format
 import pickle 
 pickle.dump(rf,open('model.pkl','wb'))
